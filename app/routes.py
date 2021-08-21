8c905068195d285538fc53c93ce37ae626b1ae65
OyugoObonyo <nyikal1999@gmail.com>
2021-08-21 11:53:53 +0300

    Dumy data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import application
+
 
 @application.route('/')
 @application.route('/home')
 def home():
-    return "Hello world!"
+    user = {'username': 'Bruno'}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Looks like a beautiful day today!"
+        },
+        {
+            "author": {"username": "Abby"},
+            "body": "Who is in town today?"
+        }     
+    ]
+    return render_template('index.html', user=user, posts=posts)
