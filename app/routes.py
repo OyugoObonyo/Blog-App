b9b3872faa2e7952bca8cbcb15d7e633a6983476
OyugoObonyo <nyikal1999@gmail.com>
2021-08-24 16:04:20 +0300

    Handle web form validation and processing

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import application
 from app.forms import LoginForm
 
+
 @application.route('/')
-@application.route('/home')
-def home():
+@application.route('/index')
+def index():
     user = {'username': 'Bruno'}
     posts = [
         {
@@ -18,7 +19,11 @@ def home():
     ]
     return render_template('index.html', user=user, posts=posts)
 
-@application.route('/login')
+@application.route('/login', methods=["GET", "POST"])
 def login():
     log_form = LoginForm()
+    if log_form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(
+            log_form.username.data, log_form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Log in', form=log_form)
