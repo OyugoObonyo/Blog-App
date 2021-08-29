d7ce84302f5e2a9981ce43086ff70b6998ea93ae
OyugoObonyo <nyikal1999@gmail.com>
2021-08-29 15:43:15 +0300

    Add user registration form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha import validators
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +9,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already exists')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('email address already in use')
