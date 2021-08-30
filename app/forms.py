02e7aacf797b3ed26a37743270dae46b66513b9f
OyugoObonyo <nyikal1999@gmail.com>
2021-08-30 17:11:57 +0300

    clear whitespace

--- app/forms.py
+++ app/forms.py
@@ -22,7 +22,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Username already exists')
-    
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
