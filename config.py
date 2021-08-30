3f96b3f8770816faee3a06d2cff0f6ac34d56dbf
OyugoObonyo <nyikal1999@gmail.com>
2021-08-30 17:11:10 +0300

    Handle pep8 warning

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedirectory = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "Never-really-been-good-at-guessing"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedirectory, 'application.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'good-at-guessing'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedirectory, 'application.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
