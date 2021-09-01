6fbedc9358740ce08abb03b0756e2e65e1a766d2
OyugoObonyo <nyikal1999@gmail.com>
2021-09-01 16:02:48 +0300

    actualize mail server configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 application = Flask(__name__)
 application.config.from_object(Config)
@@ -11,4 +14,33 @@ migrate = Migrate(application, db)
 login = LoginManager(application)
 login.login_view = 'login'
 
-from app import routes, models
+
+if not application.debug:
+    if application.config['MAIL_SERVER']:
+        auth = None
+        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
+        secure = None
+        if application.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
+            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+            toaddrs=application.config['ADMINS'], subject='BLOG APP FAILURE!',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        application.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+        from app import routes, models, errors
