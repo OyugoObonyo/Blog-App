087ff2dd96a58fc389b058555d509c98fdbc3210
OyugoObonyo <nyikal1999@gmail.com>
2021-09-01 16:06:50 +0300

    Add inheritance to instance

--- app/routes.py
+++ app/routes.py
@@ -87,7 +87,7 @@ def user(username):
 @application.route('/edit_profile', methods =['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.bio = form.bio.data
