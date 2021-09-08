fab74a2701f689e8e3fff7e4960a43dc784bca9b
OyugoObonyo <nyikal1999@gmail.com>
2021-09-08 16:03:44 +0300

    Create association table

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     bio = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -42,3 +43,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
