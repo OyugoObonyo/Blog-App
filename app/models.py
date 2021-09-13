417c50d32e6aa4c19ef0f6f34a4c31d6f6cdefc3
OyugoObonyo <nyikal1999@gmail.com>
2021-09-13 17:59:23 +0300

    Create reset password methods

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 from sqlalchemy.orm import backref
-from app import db, login
+from app import db, login, application
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 
 followers = db.Table('followers',
@@ -50,13 +52,27 @@ class User(UserMixin, db.Model):
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
-    
+
     def followed_posts(self):
         followed = Post.query.join(
            followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            application.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, application.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 @login.user_loader
 def load_user(id):
