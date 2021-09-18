fa11eb367ca27a412d76848d768025021feb775e
OyugoObonyo <nyikal1999@gmail.com>
2021-09-18 19:44:17 +0300

    Add guess_language extension

--- config.py
+++ config.py
@@ -14,3 +14,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['nyikal1999@gmail.com']
     POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
