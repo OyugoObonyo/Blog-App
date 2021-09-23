7e5aa6e8493b587b62648444c77a10284594fe89
OyugoObonyo <nyikal1999@gmail.com>
2021-09-23 18:54:30 +0300

    Register auth and errors blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, app
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -14,55 +14,70 @@ from flask_babel import lazy_gettext as _l
 from flask import request
 from app.errors import bp as errors_bp
 from app.auth import bp as auth_bp
+from app.main import bp as main_bp
 
 
-application = Flask(__name__)
-application.register_blueprint(errors_bp)
-application.register_blueprint(auth_bp)
-application.config.from_object(Config)
-db = SQLAlchemy(application)
-migrate = Migrate(application, db)
-login = LoginManager(application)
-login.login_view = 'login'
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
 login.login_message = _l('Please log in to access this page')
-mail = Mail(application)
-bootstrap = Bootstrap(application)
-moment = Moment(application)
-babel = Babel(application)
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
 
 
-if not application.debug:
-    if application.config['MAIL_SERVER']:
-        auth = None
-        if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
-            auth = (application.config['MAIL_USERNAME'], application.config['MAIL_PASSWORD'])
-        secure = None
-        if application.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(application.config['MAIL_SERVER'], application.config['MAIL_PORT']),
-            fromaddr='no-reply@' + application.config['MAIL_SERVER'],
-            toaddrs=application.config['ADMINS'], subject='BLOG APP FAILURE!',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        application.logger.addHandler(mail_handler)
+def create_app(config_class=Config):
+    application = Flask(__name__)
+    application.config.from_object(config_class)
+
+    db.init_app(application)
+    migrate.init_app(application)
+    login.init_app(application)
+    mail.init_app(application)
+    bootstrap.init_app(application)
+    moment.init_app(application)
+    babel.init_app(application)
+
+    application.register_blueprint(errors_bp)
+    application.register_blueprint(auth_bp)
+    application.register_blueprint(main_bp)
+
+    if not application.debug and not application.testing:
+        if application.config['MAIL_SERVER']:
+            auth = None
+            if application.config['MAIL_USERNAME'] or application.config['MAIL_PASSWORD']:
+                auth = (application.config['MAIL_USERNAME'],
+                        application.config['MAIL_PASSWORD'])
+            secure = None
+            if application.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(application.config['MAIL_SERVER'],
+                          application.config['MAIL_PORT']),
+                fromaddr='no-reply@' + application.config['MAIL_SERVER'],
+                toaddrs=application.config['ADMINS'], subject='BLOG APP FAILURE!',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            application.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+      '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     application.logger.addHandler(file_handler)
 
     application.logger.setLevel(logging.INFO)
     application.logger.info('Microblog startup')
 
-    
-    @babel.localeselector
-    def get_locale():
-        return request.accept_languages.best_match(application.config['LANGUAGES'])
+    return application
 
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
-    from app import routes, models
+from app import routes, models
