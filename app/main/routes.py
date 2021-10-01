67ebfe7d3abc9ee75c317034be8facf4cd281d4c
OyugoObonyo <nyikal1999@gmail.com>
2021-10-01 12:55:20 +0300

    Add flask babel import to handle error

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify, current_app
 from flask_login import current_user, login_required
-from app import db, get_locale
+from app import db
 from app.main.forms import EditProfileForm, EmptyForm, PostForm
 from app.models import User, Post
 from datetime import datetime
+from flask_babel import _, get_locale
 from guess_language import guess_language
 from app.translate import translate
 from app.main import bp
