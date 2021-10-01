bd337a8aa902e17d1ee6ce3f32474c77a7d9bf7c
OyugoObonyo <nyikal1999@gmail.com>
2021-10-01 12:31:48 +0300

    Create Searchform

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Length, ValidationError
 from app.models import User
+from flask import request
 
 
 class EditProfileForm(FlaskForm):
@@ -29,3 +30,14 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
